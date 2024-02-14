0778e8e90780032e3f1a9eeb7632f51391a7cb48
Ruxin Qu <rosheenqu@gmail.com>
2024-02-15 00:33:55 +0800

    allow cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const userRoutes = require("./routes/user-routes");
 const imageRoutes = require("./routes/image-upload.js");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  // You can also configure other CORS options here
+};
 
+app.use(cors(corsOptions));
 // express middleware, used to be bodyparser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
